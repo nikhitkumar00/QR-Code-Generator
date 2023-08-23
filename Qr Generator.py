65ff669ab679c30e152a99776e1b3e34295354d8
nikhitkumar00 <nikhitkumar00@gmail.com>
2023-08-23 22:23:27 +0530

    added manual generation

--- Qr Generator.py
+++ Qr Generator.py
@@ -6,7 +6,8 @@ qr = qrcode.QRCode(
     box_size=10,
     border=4,
 )
-qr.add_data("nikhitkumar.netlify.app")
+data = input()
+qr.add_data(data)
 qr.make(fit=True)
 
 qr_image = qr.make_image(fill_color="black", back_color="white")
